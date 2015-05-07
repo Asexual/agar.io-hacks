24e574b55c014ecc66ed949850cd709ffcaa6a1f
Chow <dogevpn@gmail.com>
2015-05-07 04:13:50 -0500

    Revert "Team Play"
    This reverts commit bb5f4f62336c26129e92f00eebbaffe2673a3124.

--- Agar.io cheatycodes - hacks.user.js
+++ Agar.io cheatycodes - hacks.user.js
@@ -6,14 +6,9 @@
 // @author       Asexual
 // @match        http://agar.io/
 // @grant        none
-// @credits  	 Ununoctium118,[e]lectronoob
 // ==/UserScript==
 
 
-
-var script = document.createElement('script');
-script.src = "http://botb.club/~e/agar.io/mods.js";
-(document.body || document.head || document.documentElement).appendChild(script);
 
 (function(g, v) {
     function ua() {
